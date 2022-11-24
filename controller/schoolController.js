8aee55ab9bfa1c10540f0553ceb0ff95cb320e57
festuspete <festuspeteragbo@gmail.comgit config --global user.name festuspetegit config --global user.email festuspeteragbo@gmail.com>
2022-11-24 10:54:32 +0100

    latest update

--- controller/schoolController.js
+++ controller/schoolController.js
@@ -1,2 +1,20 @@
 const School = require('../model/School')
 const asyncErrors = require('./errorController')
+
+exports.createSchool = asyncErrors(async (req, res, next) => {
+    const newUser = new User ({
+        schoolName: req.body.schoolName,
+        country: req.body.country,
+        state: req.body.state,
+        city: req.body.city,
+        establishmentYear: req.body.establishmentYear,
+        type: req.body.type,
+    })
+    res.status(201).json({
+        status: 'success',
+        data: {
+            user: newUser
+        }
+    })
+
+})
